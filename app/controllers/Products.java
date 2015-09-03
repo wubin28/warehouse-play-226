3545daf90dff24a2752b447e4146c8ca49882c61
Ben Wu <wubin28@gmail.com>
2015-09-03 10:43:38 +0800

    Added 4 action methods in the controller Products.

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -1,9 +1,25 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Result;
 
 /**
  * Created by twer on 9/3/15.
  */
 public class Products extends Controller {
+    public Result list() {
+        return TODO;
+    }
+
+    public Result newProduct() {
+        return TODO;
+    }
+
+    public Result details(String ean) {
+        return TODO;
+    }
+
+    public Result save() {
+        return TODO;
+    }
 }
