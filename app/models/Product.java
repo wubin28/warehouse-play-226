b316baafa11b1e8504bcccbb278ed6f519eef220
Ben Wu <wubin28@gmail.com>
2015-09-03 10:49:31 +0800

    Added some test data to /app/models/Product.java.

--- app/models/Product.java
+++ app/models/Product.java
@@ -1,5 +1,8 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by twer on 9/3/15.
  */
@@ -7,6 +10,17 @@ public class Product {
     public String ean;
     public String name;
     public String description;
+
+    private static List<Product> products;
+
+    static {
+        products = new ArrayList<Product>();
+        products.add(new Product("1111111111111", "Paperclips 1", "Paperclips description 1"));
+        products.add(new Product("2222222222222", "Paperclips 2", "Paperclips description 2"));
+        products.add(new Product("3333333333333", "Paperclips 3", "Paperclips description 3"));
+        products.add(new Product("4444444444444", "Paperclips 4", "Paperclips description 4"));
+        products.add(new Product("5555555555555", "Paperclips 5", "Paperclips description 5"));
+    }
 
     public Product() {
     }
